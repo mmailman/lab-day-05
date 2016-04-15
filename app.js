e8bd55b331a07d52476dd3a2e694fb6e4fb88d31
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-04-15 12:59:43 -0700

    created multiply() function

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ function sum(num1, num2) {
   var total = num1 + num2;
   console.log('The sum of ' + num1 + ' and ' + num2 + ' is ' + total + '.');
 }
+
+function multiply(num1, num2) {
+  var product = num1 * num2;
+  console.log('The product of ' + num1 + ' and ' + num2 + ' is ' + product + '.');
+}
