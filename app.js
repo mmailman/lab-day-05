c8a09f6c5e86872d8d819b90dc89a7b3a4bbff6b
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-04-15 13:36:02 -0700

    created sumArray() function to accept array as parameter and sum of numbers

--- app.js
+++ app.js
@@ -13,4 +13,11 @@ function sumAndMultiply(num1, num2, num3) {
   var product = multiply(multiply(num1, num2), num3);
   console.log(num1 + ' and ' + num2 + ' and ' + num3 + ' sum to ' + add + '.');
   console.log('The numbers ' + num1 + ' and ' + num2 + ' and ' + num3 + ' have a product of ' + product + '.');
+  return [add, product];
+}
+
+function sumArray(array) {
+  var sum = array[0] + array[1] + array[2];
+  console.table(array);
+  console.log(array[0] + ',' + array[1] + ',' + array[2] + ', was passed in as an array of numbers, and ' + sum + ' is their sum.');
 }
