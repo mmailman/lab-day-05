8d3b363c7f8ff86822c7992e69d6025fb190807e
Michael Mailman <mmailman@gmail.com>
2016-04-15 15:09:30 -0700

    Reviewing James lab assignment and adding comments.

---
+++ README.md
@@ -0,0 +1,3 @@
+Oops James forgot the README.
+
+This was reviewed on 04-15-2016 by Michael Mailman
--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//The sum and multply functions are very dry to the point of readability my be a bit difficult.
 function sum(num1, num2) {
   var sumEl = document.getElementById('sumEl');
   sumEl.textContent = 'The sum of ' + num1 + ' and ' + num2 + ' is ' + (num1 + num2) + '.';
@@ -10,6 +11,7 @@ function multiply(num1, num2) {
   return num1 * num2;
 }
 
+//Looking good
 function sumAndMultiply(num1, num2, num3) {
   var sumAndMultiplyEl = document.getElementById('sumAndMultiplyEl1');
   document.getElementById('sumAndMultiplyEl2');
@@ -20,13 +22,16 @@ function sumAndMultiply(num1, num2, num3) {
   return [add, product];
 }
 
+//The sumArray function here is ver brittle, it will break if there is an array that is smaller or larger than 3. I would use a for loop here.
 function sumArray(array) {
   var sumArrayEl =  document.getElementById('sumArrayEl');
   var sum = array[0] + array[1] + array[2];
   console.table(array);
+  //One thing I found out is that if you just use the array variable you will get the formatting the way specified.
   sumArrayEl.textContent = array[0] + ',' + array[1] + ',' + array[2] + ' was passed in as an array of numbers, and ' + sum + ' is their sum.';
 }
 
+//See the comment for sumArray it applies here as well.
 function multiplyArray(array) {
   var multiplyArrayEl = document.getElementById('multiplyArrayEl');
   var product = array[0] * array[1] * array[2];
